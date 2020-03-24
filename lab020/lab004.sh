1a446e195437baa42e81144235dff59947deaeaf
liguoqinjim <liguoqinjim23@gmail.com>
2020-03-24 20:14:08 +0800

    update

--- lab020/lab004.sh
+++ lab020/lab004.sh
@@ -6,6 +6,13 @@ sed -e "s/$a/$b/g" "data/temp01.txt"
 
 a="li.com/weixin/server"
 b="github.com/liguoqinjim/iris_template"
-sed -e 's,'"$a"','"$b"',g' "data/temp03.txt"
+# -e
+# sed -e 's,'"$a"','"$b"',g' "data/temp03.txt"
+
+# -i
+# sed -i '' 's,'"$a"','"$b"',g' "data/temp03.txt"
+
+# $1
+sed -i '' 's,'"$a"','"$1"',g' "data/temp03.txt"
 
 # sed -i '' "s/li.com\/weixin\/server/github.com\/liguoqinjim\/iris_template/g" "data/temp03.txt"
